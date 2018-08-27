08fd320ad38d6b5c21845f55591d075ff2b823a1
Asus <Asus@ASUSDIEGO>
2018-08-26 21:09:44 -0500

    Lenguaje para ciegos

--- src/unal/poo/practica/RobotBase.java
+++ src/unal/poo/practica/RobotBase.java
@@ -19,30 +19,66 @@ public class RobotBase
             
             //Direction.NORTH, EAST, SOUTH, WEST
             //Definicion de la ubicacion del robot, Ciudad, posicion, Direccion, Numero things en el bolso.
-            estudiante = new Robot(objetos,0, 1, Direction.SOUTH,0);
-            for(int i=0;i<5;i+=1){
-                estudiante.move ();
-                while(estudiante.canPickThing()==true){
-                    estudiante.pickThing();
-                }
-                estudiante.turnLeft();
-                int x=estudiante.countThingsInBackpack();
-                for(int j=0;j<x;j+=1){
-                    estudiante.putThing();
-                    estudiante.move();
-                }
-                darVuelta();
-                for(int k=0;k<x;k+=1){
-                    estudiante.move();
-                }
-                estudiante.turnLeft();
+            estudiante = new Robot(objetos,0, 0, Direction.SOUTH,0);
+            int x=0;
+            char[] xd=new char[88];
+            estudiante.move();
+            if(estudiante.canPickThing()==true){
+                x=1;
             }
+            estudiante.move();
+            if(estudiante.canPickThing()==true){
+                x+=4;
+            }
+            estudiante.move();
+            if(estudiante.canPickThing()==true){
+                x+=9;
+                xd[26]='M';
+                xd[30]='P';
+            }
+            estudiante.turnLeft(); 
+            estudiante.move();
+            if(estudiante.canPickThing()==true){
+                x+=36;
+            }
+            estudiante.turnLeft();
+            estudiante.move();
+            if(estudiante.canPickThing()==true){
+                x+=25;
+            }
+            estudiante.move();
+            if(estudiante.canPickThing()==true){
+                x+=16;
+            }
+            
+            xd[1]='A';
+            xd[5]='B';
+            xd[17]='C';
+            xd[41]='D';
+            xd[26]='E';
+            xd[21]='F';
+            xd[46]='G';
+            xd[30]='H';
+            xd[20]='I';
+            xd[45]='J';
+            xd[10]='K';
+            xd[14]='L';
+            xd[51]='N';
+            xd[35]='O';
+            xd[55]='Q';
+            xd[39]='R';
+            xd[29]='S';
+            xd[54]='T';
+            xd[46]='U';
+            xd[50]='V';
+            xd[81]='W';
+            xd[62]='X';
+            xd[87]='Y';
+            xd[71]='Z';
+       
+            System.out.println(xd[x]);
+            
         }
-        public static void darVuelta(){
-            for (int i = 0; i < 2; i++){
-                estudiante.turnLeft();
-            };
-        }
-        
+       
 }
 
